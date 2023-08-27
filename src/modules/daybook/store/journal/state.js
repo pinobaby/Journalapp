64d750df24b66ce395193a78588ebcbffc88f5b1
pinobaby <angel.ismael684@gmail.com>
2023-08-26 19:44:39 -0700

    add entrues

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -8,6 +8,20 @@ export default () => ({
             text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
             picture: null
             
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Consectetur laboris excepteur cupidatat proident laboris velit aliquip culpa ipsum aliquip ut ex. Excepteur occaecat adipisicing proident laborum. Ullamco magna sit culpa tempor sit minim occaecat elit. Enim reprehenderit id occaecat irure amet tempor. Nostrud quis eu ut incididunt. Consequat eiusmod ipsum dolore culpa qui voluptate non aliqua dolor laborum mollit. Dolore laborum et esse sint irure laborum excepteur est nostrud sint.',
+            picture: null
+            
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Enim do ea aute tempor sunt. Id non commodo ipsum officia. Eiusmod do commodo duis ipsum amet do occaecat nisi veniam veniam in est. Aute velit dolore magna laboris ea aute adipisicing quis incididunt nisi commodo laboris nisi sunt. Irure ullamco nisi minim Lorem qui do non amet exercitation pariatur non excepteur in.',
+            picture: null
+            
         }
         
     ]
