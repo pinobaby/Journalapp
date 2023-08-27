2ba9b693f170dd89acb7d2346c0510204fdbc425
pinobaby <angel.ismael684@gmail.com>
2023-08-26 19:50:58 -0700

    fix state

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -3,10 +3,12 @@ export default () => ({
     isLoading: true,
     entries: [
         {
+    
             id: new Date().getTime(),
             date: new Date().toDateString(),
             text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
             picture: null
+            
             
         },
         {
